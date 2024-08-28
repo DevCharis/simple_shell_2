2134f59aaeb45b305b2e8963cb5eaba01ebdf0f6
Joshua Bamidele <joshuabamidele9@gmail.com>
2024-08-27 23:37:50 -0700

    lets try this headers

--- execute_command.c
+++ execute_command.c
@@ -1,3 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <errno.h>
 #include "shell.h"
 
 /**
